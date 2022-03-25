6c51b6a696bc470dca066c55c71e9dff04c9bc19
AlessandroFolini <101130184+AlessandroFolini@users.noreply.github.com>
2022-03-25 19:30:42 +0100

    Modified the value of maxMNSteps for Lizard and Ostrich

--- src/main/java/it/polimi/ingsw/model/Assistant.java
+++ src/main/java/it/polimi/ingsw/model/Assistant.java
@@ -2,8 +2,8 @@ package it.polimi.ingsw.model;
 
 public enum Assistant {
     EAGLE(4, 2), DOG(8, 4), ELEPHANT(9, 5), CAT(3, 2),
-    CHEETAH(1, 1), LIZARD(6, 4), OCTOPUS(7, 4), TURTLE(10, 5),
-    FOX(5, 3), OSTRICH(2, 4);
+    CHEETAH(1, 1), LIZARD(6, 3), OCTOPUS(7, 4), TURTLE(10, 5),
+    FOX(5, 3), OSTRICH(2, 1);
 
     private final int weight;
     private final int maxMNSteps;
