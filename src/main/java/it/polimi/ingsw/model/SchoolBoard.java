d05c2e86cc6457d0bd7d701921dbabbb2e5ae79b
robertogiandomenico <roberto.giandomenico@mail.polimi.it>
2022-06-21 14:36:32 +0200

    minor fixes

--- src/main/java/it/polimi/ingsw/model/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/model/SchoolBoard.java
@@ -299,7 +299,7 @@ public class SchoolBoard {
                         // case opponent not found, I ipso facto take the professor
                         professorTable[index] = true;
                     } else if((diningRoom[index] > enemyBoard.getDiningRoom()[index]) ||
-                              (diningRoom[index] == enemyBoard.getDiningRoom()[index] && farmerEffect && diningRoom[index] != 0)){
+                              (diningRoom[index] == enemyBoard.getDiningRoom()[index])) {
                         // case opponent found and I have more students OR I have equal students with farmerEffect active
                         professorTable[index] = true;
                         enemyBoard.setProfessor(index, false);
