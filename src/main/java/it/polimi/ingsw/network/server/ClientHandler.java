5a24f53546a001636e800ece3c37f9d1c47e927b
alessandrofolini <alessandro.folini@mail.polimi.it>
2022-06-30 12:47:15 +0200

    minor fixes

--- src/main/java/it/polimi/ingsw/network/server/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/network/server/ClientHandler.java
@@ -14,6 +14,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 
 /**
@@ -75,7 +76,7 @@ public class ClientHandler implements Runnable{
                         GenericClientMessage msg = (GenericClientMessage) object;
                         msg.execute(server, this);
                     }
-                } catch (ClassNotFoundException e) {
+                } catch (ClassNotFoundException | SocketTimeoutException e) {
                     manageDisconnection();
                 }
 
